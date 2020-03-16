ee471b763e278e8f662aeffeca86346858fc6381
KoHanjun <rhgkswns@naver.com>
2020-03-16 17:40:12 +0900

    Make o file list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello World')
+import os
+
+print('Start Search...')
+print('Make o file list')
+
+for (path, dir, files) in os.walk("E:/MyFolder/Source\eclipse/test200313"):
+    for filename in files:
+        ext = os.path.splitext(filename)[-1]
+        if ext == '.c' or ext == '.cpp' or ext =='.S' or ext == '.s':
+            print("%s/%s.o" % (path, filename.split('.')[0]))
+
